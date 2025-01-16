e8331fbfefcc1191bb2af81db64d5fb4f67d2aa3
Katarina Bjegovic <kbjegovi@ucsc.edu>
2025-01-15 18:05:46 -0800

    edit js file

--- assignments/asgn0/asgn0.js
+++ assignments/asgn0/asgn0.js
@@ -11,6 +11,8 @@ function main() {
 // Draw a blue rectangle
     ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set a blue color 
     ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+    //create ector here
+    v1 = new Vector3([2, 3, 0])
     drawVector(v1, "red")
 }
 
